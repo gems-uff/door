98850c400ec0705bd4081b04ec394ed083f5d0d4
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2014-10-12 09:41:06 -0300

    Printing what the script is doing

--- door.py
+++ door.py
@@ -3,10 +3,12 @@ import RPi.GPIO as GPIO
 DOOR = 17 # GPIO id (BCM) to control the door
 OPEN_INTERVAL = 1 # Time interval sending open signal (in seconds)
 
-def open_door:
+def open_door():
+    print('Sending signal to open the door...')
     GPIO.output(DOOR, GPIO.HIGH)
     time.sleep(OPEN_INTERVAL)
     GPIO.output(DOOR, GPIO.LOW)
+    print('Finished.')
 
 # Setup GPIO 17 (BCM) for opening the door
 GPIO.setmode(GPIO.BCM)

282804ae876f396cf75f423e43c64524c460b254
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2014-10-12 16:06:31 -0300

    Allowing execution outside Raspberry Pi

--- door.py
+++ door.py
@@ -1,18 +1,27 @@
 import time
-import RPi.GPIO as GPIO
+try:
+    import RPi.GPIO as GPIO
+    RASPBERRY_PI = True
+except ImportError:
+    RASPBERRY_PI = False
 
 DOOR = 17 # GPIO id (BCM) to control the door
 OPEN_INTERVAL = 1 # Time interval sending open signal (in seconds)
 
 def open_door():
-    print('Sending signal to open the door...')
-    GPIO.output(DOOR, GPIO.HIGH)
-    time.sleep(OPEN_INTERVAL)
-    GPIO.output(DOOR, GPIO.LOW)
-    print('Finished.')
+    if RASPBERRY_PI:
+        GPIO.output(DOOR, GPIO.HIGH)
+        time.sleep(OPEN_INTERVAL)
+        GPIO.output(DOOR, GPIO.LOW)
+    else:
+        print('Opening the door.')
 
-# Setup GPIO 17 (BCM) for opening the door
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(DOOR, GPIO.OUT, initial=GPIO.LOW)
+if RASPBERRY_PI:
+    # Setup GPIO 17 (BCM) for opening the door
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(DOOR, GPIO.OUT, initial=GPIO.LOW)
 
 open_door()
+
+if RASPBERRY_PI: GPIO.cleanup(DOOR)
